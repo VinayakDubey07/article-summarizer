18a9228a0ecb5925bfca3d20b71efb08656fb8ee
VinayakDubey <vinayakdubey0108@gmail.com>
2023-05-02 18:39:37 +0530

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,11 +20,11 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
-        <span className='blue_gradient '>OpenAI GPT-4</span>
+        Summarize Articles in <br className='max-md:hidden' />
+        <span className='blue_gradient '>One Click</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with an open-source article summarizer
         that transforms lengthy articles into small summaries within seconds!
       </h2>
     </header>
